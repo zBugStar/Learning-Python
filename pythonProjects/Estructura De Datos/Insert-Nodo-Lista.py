1ffde9836926f803acf061fb77e467d3c30ecb0b
Juan Diego Orozco Meza <161064571+zBugStar@users.noreply.github.com>
2024-09-16 17:16:25 -0500

    Insert exercise

--- pythonProjects/Estructura De Datos/Insert-Nodo-Lista.py
+++ pythonProjects/Estructura De Datos/Insert-Nodo-Lista.py
@@ -1,39 +1,42 @@
-class nodo:
-    def __init__(self, dato):
-        self.dato = dato
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
         self.siguiente = None
 
-class listaEnlazada:
 
+class ListaEnlazada:
     def __init__(self):
         self.cabeza = None
 
-    def ordenamiento(self, lista):
+    def insertar(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if self.cabeza is None or self.cabeza.valor >= nuevo_nodo.valor:
+            nuevo_nodo.siguiente = self.cabeza
+            self.cabeza = nuevo_nodo
+        else:
+            actual = self.cabeza
+            while actual.siguiente is not None and actual.siguiente.valor < nuevo_nodo.valor:
+                actual = actual.siguiente
+            nuevo_nodo.siguiente = actual.siguiente
+            actual.siguiente = nuevo_nodo
 
-        while lista:
-            if self.cabeza is None or self.cabeza.dato >= lista.dato:
-                lista.siguiente = self.cabeza
-                self.cabeza = lista
-            else:
-                actual = self.cabeza
-                while actual.siguiente is not None and actual.siguiente.dato < lista.dato:
-                    actual = actual.siguiente
-                lista.siguiente = actual.siguiente
-                actual.siguiente = lista
-            lista = lista.siguiente
-
-    def imprimir(self):
+    def to_list(self):
+        resultado = []
         actual = self.cabeza
-        while actual:
-            print(actual.dato, end=" -> ")
+        while actual is not None:
+            resultado.append(actual.valor)
             actual = actual.siguiente
-        print("None")
+        return resultado
 
 
-lista = ListaEnlazada()
-lista.insertar(5)
-lista.insertar(3)
-lista.insertar(2)
-lista.insertar(1)
-lista.insertar(10)
-lista.insertar(6)
+def ordenamiento_por_insercion(vector):
+    lista = ListaEnlazada()
+    for valor in vector:
+        lista.insertar(valor)
+    return lista.to_list()
+
+
+# Ejemplo de uso
+vector = [4, 2, 7, 1, 3]
+ordenado = ordenamiento_por_insercion(vector)
+print(ordenado)  # Salida: [1, 2, 3, 4, 7]
